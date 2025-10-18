d0209c40724372addfcd72102f9dfc03cc1a132b
nguyenhongdien <132563944+nguyenhongdien@users.noreply.github.com>
2025-10-18 09:26:22 +0700

    first

---
+++ AI_4_SE_BattleShip/AI_4_SE_BattleShip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AI_4_SE_BattleShip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
